92f8557e0054372787878798c7b188c9c125e833
HackerGal <mmolifie@gmail.com>
2017-06-16 16:23:57 +0200

    Second attempt at changes to triangle memes

--- main.cpp
+++ main.cpp
@@ -1,58 +1,74 @@
 #include <iostream>
-#include <iomanip>   // format manipulation
 
-using namespace std;
-/**This program displays an equilateral triangle based on the user's input.
- * The user decides on its size and the character that it may consist of.
- * The triangle increases by three characters each time and is based on the 
- * T(n)=2n+1 equation.*/
-void inputCheck(int & size)//Still trying to figure our cin.fail() and cin.good()
-{
- bool isdigit();
-    if(size.isdigit()==true)
-        
-        cin.good();
-    cout<<endl;
-        
-    else
-        cin.fail();
-        cout << "Please re-enter a size. ";
-        cin.clear();
-        cin>>size;
-}
+ using namespace std;
+
+/** 
+* The purpose of this function is to displays an equilateral
+* triangle based on the user's input.
+* The user decides on its size and the character that it may consist of.
+* The triangle increases by three characters each time and is based on the
+* T(n)=2n+1 equation.
+* @param char character The character that the user enters.
+* @param int size The size of the triangle.
+*/
+
 void generateEquilateralTriangle(char character, int size)
 {
-     for (int i = 0; i < size; i++)
-    {
-        
-        for (int j = 0; j < (2*size+1); j++)
-        {
-            
-            if ((size+i) >= j && (size-i) <= j)
-             
-                cout << character;
-            
-            else
-              
-                cout << " ";
-            
-        } 
+
+     for (int i = 0; i < size; i++){
+
+        for (int j = 0; j < (2*size+1); j++){
+
+            cout << (((size+i) >= j && (size-i) <= j)?character: ' ');
+
+        }
+
         cout << endl;
-    }
+
+     }
 }
-/**The user inputs their request in the main function.*/
+
+/**
+* The purpose of this function is to validate the user's input.
+* @param char character The character that the user enters.
+* @param int size The size of the triangle.
+* @return True if input is invalid.
+*/
+
+bool validateInput(char character,int size)
+{
+    if (cin.fail()){
+
+        cin.clear();
+        cin.ignore();
+    }
+    else
+        generateEquilateralTriangle(character,size);
+
+}
+
+/**
+ * The purpose of this method is to manage user input in order to
+ * correctly display the triangle.
+ *
+ * @author Chad Petersen
+ * @return The valid input and display the triangle.
+ */
+
 int main()
 {
 int size;
 char character;
 
-    cout << "Enter a size for your triangle: " << endl;
-    cin  >> size;
-    cout << "Enter a character for your triangle: " << endl;
-    cin  >> character;
-    
-    inputCheck();
-    generateEquilateralTriangle(character, size);
-      
+    do{
+
+        cout << "Enter a size for your triangle: " << endl;
+        cin  >> size;
+        cout << "Enter a character for your triangle: " << endl;
+        cin  >> character;
+
+    }while(validateInput(character,size)!=0);
+
     return 0;
-}
+
+ }
